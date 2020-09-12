777977393b0f25281eeaa14156c24f44ff7aadf5
nahid <nahidkishore99@gmail.com>
2020-09-12 13:41:52 +0600

    google login auth setup

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEYsuYz2GGG5DnH-Vbm-F6W5Z070H9yu0",
+  authDomain: "burjh-al-arab.firebaseapp.com",
+  databaseURL: "https://burjh-al-arab.firebaseio.com",
+  projectId: "burjh-al-arab",
+  storageBucket: "burjh-al-arab.appspot.com",
+  messagingSenderId: "868122144962",
+  appId: "1:868122144962:web:d0142d627852870fb08b5b"
+};
+export default firebaseConfig;
